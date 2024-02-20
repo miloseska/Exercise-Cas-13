a270c4e28d34e70ccb9398c395a376f861b79942
Bojana Miloseska <bojanamiloseska28@gmail.com>
2024-02-20 20:16:20 +0100

    second commit

--- register.js
+++ register.js
@@ -5,12 +5,38 @@ let modalBody = document.getElementsByClassName("modal-body")[0];
 
 register.addEventListener('click' , () =>{
 modalTitle.innerHTML = 'Register';
+
 modalBody.innerHTML = createRegisterForm();
+let submitBtn = document.getElementById("submitBtn");
+
+let formIsValid = isValid();
+
+if(formIsValid) {
+  submitBtn.removeAttribute("disabled");
+}
+
+submitBtn.addEventListener("click", (e) =>{
+  e.preventDefault();
+  let inputArray = document.querySelectorAll("input");
+    let emptyField = false;
+    inputArray.forEach((element) => {
+      if (element.value === "") {
+        emptyField = true;
+        return;
+      }
+    });
+
+    if (emptyField) {
+      let errorAll = document.getElementById("errorAll");
+      errorAll.innerText = "Please fill all of the fields";
+      return;
+    }
+    postForm();
+})
 })
 
 function createRegisterForm(){
   return `
-  <form id = "registerForm">
   <form class="row g-3 needs-validation dis-flex_justcont-cent width-100" novalidate id="myForm">
   <div class="error" id="errorAll"></div>  
   <div class="col-md-7">
@@ -51,8 +77,43 @@ function createRegisterForm(){
   </div>
   </div>
     <div class="col-12 dis-flex_justcont-cent">
-      <button data-bs-dismiss="modal" class="btn btn-primary" id="submitBtn" type="submit">Submit form</button>
+      <button data-bs-dismiss="modal" class="btn btn-primary" id="submitBtn" disabled type="submit">Submit form</button>
     </div>
   </form>
   `
+}
+
+function postForm(){
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let city = document.getElementById("city").value;
+  let email = document.getElementById("email").value;
+  let job = document.getElementById("job").value;
+  let password = document.getElementById("password").value;
+
+  const user = {
+    firstname : firstName,
+    lastname : lastName,
+    city : city,
+    email : email,
+    job : job,
+    password : password,
+  }
+
+  const postData = JSON.stringify(user);
+
+  fetch("https://65d38018522627d50109056a.mockapi.io/api/users", {
+    method: "POST",
+    body: postData,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((response) => {
+    console.log(response);
+    if (response.ok){
+      let form = document.getElementById("myForm");
+      form.reset();
+    }
+    return response.json();
+  });
 }
